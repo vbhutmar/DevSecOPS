a471c4839c4cd15c121d5d9fa54c0c16d41e0bbb
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-11-27 02:17:36 +0530

    Update aws_instance.tf

--- hackathonpro/aws_instance.tf
+++ hackathonpro/aws_instance.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t2.micro"
-
+   ami           = "${lookup(var.ami_id, var.region)}"
+   instance_type = "t2.micro"
  }
   
   user_data = <<-EOF
