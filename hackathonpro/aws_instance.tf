f38d8261c052be8635f70ae8757c3e55637ec540
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-11-27 01:32:22 +0530

    Update aws_instance.tf

--- hackathonpro/aws_instance.tf
+++ hackathonpro/aws_instance.tf
@@ -1,4 +1,33 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
-   instance_type = "t2.micro"
- }
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  vpc_security_group_ids = [
+    module.ec2_sg.this_security_group_id,
+    module.dev_ssh_sg.this_security_group_id
+  ]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    project = "devsecops"
+  }
+
+  key_name                = "hackathon_demo"
+  monitoring              = true
+  disable_api_termination = false
+  ebs_optimized           = true
+}
