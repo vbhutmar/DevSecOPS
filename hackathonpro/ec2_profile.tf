85333fb32289ee18d2d8b841405230217fe0c2db
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-11-27 01:28:38 +0530

    Create ec2_profile.tf

---
+++ hackathonpro/ec2_profile.tf
@@ -0,0 +1,49 @@
+esource "aws_iam_role" "ec2_role_devsecops" {
+  name = "ec2_role_devsecops"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    project = "devsecops"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_profile_devsecops" {
+  name = "ec2_profile_devsecops"
+  role = aws_iam_role.ec2_role_devsecops.name
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = aws_iam_role.ec2_role_devsecops.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchGetImage",
+        "ecr:GetDownloadUrlForLayer"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
