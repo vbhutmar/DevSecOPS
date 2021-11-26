2fe63c39d8bfc4dff68a840a363d6e4db5f7a2fb
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-11-27 02:02:55 +0530

    Update aws_instance.tf

--- hackathonpro/aws_instance.tf
+++ hackathonpro/aws_instance.tf
@@ -27,10 +27,8 @@ resource "aws_instance" "web" {
     sudo chmod +x /usr/local/bin/docker-compose
   EOF
   
-  vpc_security_group_ids = [
-    module.ec2_sg.name,
-    module.dev_ssh_sg.name
-  ]
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+  
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
