c757d43a0ac3e4de0a7d811d5344af319595b685
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-12-03 00:22:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From SteallantisOPSTigers Team" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "This is the DevSecOps Demo" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works!, Have a NICE DAY" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
