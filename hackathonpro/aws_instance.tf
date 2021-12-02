cff2a386361f3ff3fb5e0a1901eb103c49f410ef
vbhutmar <71522725+vbhutmar@users.noreply.github.com>
2021-12-03 02:02:00 +0530

    Update aws_instance.tf

--- hackathonpro/aws_instance.tf
+++ hackathonpro/aws_instance.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "web" {
       sudo usermod -a -G docker ec2-user
       sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
       sudo chmod +x /usr/local/bin/docker-compose
-      sudo docker pull vgb5555/hackathonN
-      sudo docker run -itd -p 80:80 --name hackathon_demo vgb5555/hackathonN
+      sudo docker pull vgb5555/hackathonn
+      sudo docker run -itd -p 80:80 --name hackathon_demo vgb5555/hackathonn
       sudo curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=NRAK-EDR9ZIOR9VKOH48JBVM1SRO4TZV NEW_RELIC_ACCOUNT_ID=3353794 NEW_RELIC_REGION=EU /usr/local/bin/newrelic install Y     
    EOF
    
